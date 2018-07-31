75f51b2b766e4ea161966f6738c25853438f16e2
Rose Parker <parker.rose@ymail.com>
2018-07-31 00:42:43 +0000

    end of 90 minute challenge

---
+++ bowling_game.js
@@ -0,0 +1,9 @@
+function bowling_score(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u) {
+    var score_1 = 0;
+    for (var i = 0; i < 21; i++) {
+        
+    }
+    return score_1;
+}
+console.log(bowling_score(1,4,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,2,8,6));
+
